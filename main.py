d6d1c1980e9110b5f50429f4bdc6f932f3e77600
Garrett Leber <lebergarrett@gmail.com>
2023-09-03 15:48:35 -0400

    command descriptions

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ DISCORD_TOKEN = str(os.environ.get("DISCORD_TOKEN"))
 bot = discord.Bot()
 
 # create Slash Command group with bot.create_group
-greetings = bot.create_group("greetings", "Greet people")
+minecraft= bot.create_group("minecraft", "Manage Minecraft Server(s)")
 
-@greetings.command()
-async def hello(ctx):
-  await ctx.respond(f"Hello, {ctx.author}!")
+@minecraft.command()
+async def status(ctx):
+    """Get the Minecraft Server Status"""
+    await ctx.respond(f"{ctx.author}, the mc server is doing great.")
 
-@greetings.command()
-async def bye(ctx):
-  await ctx.respond(f"Bye, {ctx.author}!")
+@minecraft.command()
+async def start(ctx):
+    """Start the Minecraft Server"""
+    await ctx.respond(f"{ctx.author}, the mc server has been started.")
+
+@minecraft.command()
+async def stop(ctx):
+    """Stop the Minecraft Server"""
+    await ctx.respond(f"{ctx.author}, the mc server has been stopped.")
 
 bot.run(DISCORD_TOKEN)
 
