edcacf1c31e86e41cb4270a35e25125f99ff123e
Garrett Leber <lebergarrett@gmail.com>
2023-09-03 20:19:53 -0400

    more progress

--- main.py
+++ main.py
@@ -5,30 +5,52 @@ import os
 
 from dotenv import load_dotenv
 
+# Load configuration from environment variables/.env file
 load_dotenv()
-
 DISCORD_TOKEN = str(os.environ.get("DISCORD_TOKEN"))
-#DISCORD_GUILD_ID = str(os.environ.get("DISCORD_GUILD_ID"))
+DEFAULT_SERVER = str(os.environ.get("DEFAULT_SERVER"))
 
+# prepare bot
 bot = discord.Bot()
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
 
-# create Slash Command group with bot.create_group
-minecraft= bot.create_group("minecraft", "Manage Minecraft Server(s)")
+# create commands/command group
+minecraft = discord.SlashCommandGroup("minecraft", "Manage Minecraft server(s).")
+server_desc = "Server to target."
 
 @minecraft.command()
-async def status(ctx):
-    """Get the Minecraft Server Status"""
+async def list(ctx):
+    """
+    Get the list of Minecraft servers available to manage.
+    """
     await ctx.respond(f"{ctx.author}, the mc server is doing great.")
 
 @minecraft.command()
-async def start(ctx):
-    """Start the Minecraft Server"""
-    await ctx.respond(f"{ctx.author}, the mc server has been started.")
+async def status(ctx, server: discord.Option(str, server_desc, required=False, default=DEFAULT_SERVER)):
+    """
+    Get Minecraft server status.
+    """
+    await ctx.respond(f"{ctx.author}, the mc server {server} is doing great.")
 
 @minecraft.command()
-async def stop(ctx):
-    """Stop the Minecraft Server"""
-    await ctx.respond(f"{ctx.author}, the mc server has been stopped.")
+async def start(ctx, server: discord.Option(str, server_desc, required=False, default=DEFAULT_SERVER)):
+    """
+    Start the Minecraft server
+    """
+    await ctx.respond(f"{ctx.author}, the mc server {server} has been started.")
 
+@minecraft.command()
+async def stop(ctx, server: discord.Option(str, server_desc, required=False, default=DEFAULT_SERVER)):
+    """
+    Stop the Minecraft server
+    """
+    await ctx.respond(f"{ctx.author}, the mc server {server} has been stopped.")
+
+
+# main
+bot.add_application_command(minecraft)
 bot.run(DISCORD_TOKEN)
 
